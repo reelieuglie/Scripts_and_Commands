c174425e99ac4f306fa448424282dd3810ff38d2
reelieuglie <66641726+reelieuglie@users.noreply.github.com>
2024-11-20 10:44:03 -0500

    Update ssl_checker.sh

--- bash/ssl_checker.sh
+++ bash/ssl_checker.sh
@@ -4,12 +4,19 @@
 url=$1
 port=$2
 declare -a certlist 
+sslcmd=$(which openssl)
+
+if ! command -v openssl > /dev/null; then
+	echo "The command openssl is not in the \$PATH or not installed. Please fix. Exiting."
+	exit 1
+fi
+
 getcerts () {
-	openssl s_client -showcerts -verify 5 -connect $url:$port < /dev/null 2>/dev/null |  awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{ if(/BEGIN CERTIFICATE/){a++}; out="cert"a".pem"; print >out}' 
+	$sslcmd s_client -showcerts -verify 5 -connect $url:$port < /dev/null 2>/dev/null |  awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{ if(/BEGIN CERTIFICATE/){a++}; out="cert"a".pem"; print >out}' 
 }
 renamecerts () {
 for cert in *.pem; do
-   newname=$(openssl x509 -noout -subject -in $cert | sed -nE 's/.*CN ?= ?(.*)/\1/; s/[ ,.*]/_/g; s/__/_/g; s/_-_/-/; s/^_//g;p' | tr '[:upper:]' '[:lower:]').pem
+   newname=$($sslcmd x509 -noout -subject -in $cert | sed -nE 's/.*CN ?= ?(.*)/\1/; s/[ ,.*]/_/g; s/__/_/g; s/_-_/-/; s/^_//g;p' | tr '[:upper:]' '[:lower:]').pem
    mv $cert $newname
    certlist+=("$newname")
    export certlist
@@ -21,10 +28,28 @@ checkocsp () {
    for i in ${certlist[@]}
    do 
    echo " ++ Checking Certificate $i ++ "
-   openssl x509 -in $i -noout -ocsp_uri	 
+   $sslcmd x509 -in $i -noout -ocsp_uri	 
 done
+}
+
+cleanup () {
+  for i in ${certlist[@]}
+   do
+   read -p "Do you want to delete $i: " yn
+   case $yn in
+     [Yy]* ) echo "Deleting $i";  rm $i && echo "Deleted $i" || echo "Failed to delete $i";;
+     [Nn]* ) echo "Not deleting $i";;
+     * ) echo "Please answer yes or no.";;
+    esac
+    done
 }
 
 getcerts
 renamecerts
 checkocsp
+cleanup 
+
+getcerts
+renamecerts
+checkocsp
+cleanup
